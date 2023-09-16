b0e4a37c8bcc96baf052fccaf3ae09a1528f5b64
Jason Muteham <jason@jasonmuteham.com>
2023-09-16 13:41:15 +0100

    token check

--- src/database.py
+++ src/database.py
@@ -3,8 +3,23 @@ import logging
 from pathlib import Path
 import os
 
-def connect(db, db_name, dbpath = None):
 
+def connect(db, db_name, dbpath=None):
+    """
+    Connects to a database and returns a connection object.
+
+    Parameters:
+        db (str): The type of database to connect to. Valid values are "duckdb" and any other string.
+        db_name (str): The name of the database.
+        dbpath (str, optional): The path to the database folder. Defaults to None.
+   
+    Returns:
+        Connection: A connection object for the connected database.
+
+    Raises:
+        Exception: If there is an error creating the database folder.
+        Exception: If there is an error connecting to the database.
+    """
     if db == "duckdb":
         try:
             Path(dbpath).mkdir(exist_ok=True)
@@ -18,8 +33,8 @@ def connect(db, db_name, dbpath = None):
             print(e)
     else:
         # Need motherduck code in here
-        MOTHERDUCK_DB = os.getenv('MOTHERDUCK_DB')
-        MOTHERDUCK_TOKEN = os.getenv('MOTHERDUCK_TOKEN')
+        MOTHERDUCK_DB = os.getenv("MOTHERDUCK_DB")
+        MOTHERDUCK_TOKEN = os.getenv("MOTHERDUCK_TOKEN")
         try:
             return duckdb.connect(f"md:{MOTHERDUCK_DB}?{MOTHERDUCK_TOKEN}")
         except Exception as e:
@@ -27,9 +42,7 @@ def connect(db, db_name, dbpath = None):
             print(e)
 
 
-
-
-def df_load(conn, df_upload, sqlfile, sqlschema, sqlwrite = "append"):
+def df_load(conn, df_upload, sqlfile, sqlschema, sqlwrite="append"):
     try:
         sql = f"CREATE SCHEMA IF NOT EXISTS {sqlschema}"
         conn.sql(sql)
@@ -42,7 +55,7 @@ def df_load(conn, df_upload, sqlfile, sqlschema, sqlwrite = "append"):
             sql = f"CREATE OR REPLACE TABLE {sqlschema}.{sqlfile} AS SELECT * FROM df_upload"
         else:
             sql = f"INSERT INTO {sqlschema}.{sqlfile} SELECT * FROM df_upload"
-            
+
         logging.info(f"- SQL: {sql}")
         conn.sql(sql)
 
--- src/env_setup.py
+++ src/env_setup.py
@@ -1,11 +1,11 @@
 import os
 import tomllib
 
-def load():
-    MOTHERDUCK_DB = os.getenv('MOTHERDUCK_DB')
-    MOTHERDUCK_TOKEN = os.getenv('MOTHERDUCK_TOKEN')
 
-    if MOTHERDUCK_DB is None:
+def load():
+    MOTHERDUCK_TOKEN = os.getenv("MOTHERDUCK_TOKEN")  # noqa: F841
+
+    if MOTHERDUCK_TOKEN is None:
         # Set environment variables
         try:
             with open("config/secret.toml", "rb") as f:
@@ -14,5 +14,5 @@ def load():
             print(f"*** Can not find the secret hiding place! ***\n{e}")
             raise
 
-        os.environ['MOTHERDUCK_DB'] = shush['MOTHERDUCK_DB']
-        os.environ['MOTHERDUCK_TOKEN'] = shush['MOTHERDUCK_TOKEN']
+        os.environ["MOTHERDUCK_DB"] = shush["MOTHERDUCK_DB"]
+        os.environ["MOTHERDUCK_TOKEN"] = shush["MOTHERDUCK_TOKEN"]
