4bca8eb3fcf2c37293aa576f939ba49fa7bf2126
ancientwrangler <ancientwrangler@gmail.com>
2023-11-22 12:34:27 +0000

    pandas-csv

--- src/simple_pipe.py
+++ src/simple_pipe.py
@@ -3,6 +3,7 @@ import pond
 import logging
 import tomllib
 import custom # put your custom code in this module # noqa: F401
+import pandas as pd
 
 if __name__ == "__main__":
     print(f"{'*'*50}\nSearching for my duck pond....")
@@ -135,7 +136,32 @@ if __name__ == "__main__":
                      logging.info(f"- SQL filtering: {sql_filter_name}")
 
                 pond.load(db_con,sql_table,pipeline["schema"], schema_from="staging",sql_write=sql_write, sql_filter= sql_filter)  # noqa: E501
-            
+            #
+            # If duckdb csv loading is not working, use pandas! 
+            #
+            elif tasks[task]["file_type"] == "pandas_csv":
+                url=tasks[task]["url"]
+                try:
+                    Path(tasks[task]['file_path']).mkdir(exist_ok=True)
+                except Exception as e:
+                    print(e)
+                    raise
+                
+                try:
+                    df_download = pd.read_csv(url)
+                except Exception as e:
+                    print(e)
+                    raise
+
+                if df_download.empty:
+                    logging.warning(f"- {task}: No raw data extracted")
+                    failed_tasks += 1
+                    print("Oh No! pipe went out...")
+                else:
+                    file_path = f"{tasks[task]['file_path']}/{tasks[task]['file_name']}"
+                    df_download.to_csv(file_path, index=False)
+                    logging.info(f"- {task}: Result: {df_download.shape} extracted")
+
             elif tasks[task]["file_type"].split(".")[0] == "function":
                 """
                 your custom function call 'function.'<module>.<function>
